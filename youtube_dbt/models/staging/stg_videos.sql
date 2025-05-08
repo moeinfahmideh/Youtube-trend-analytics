89b8a36be9f6198e220ecbe51fcaa832509d6cbf
moein fahmide <fahmide.moein@gmail.com>
2025-05-08 11:19:59 +0100

    staging intermediate videos table

---
+++ youtube_dbt/models/staging/stg_videos.sql
@@ -0,0 +1,18 @@
+
+{{ config(materialized='table') }}
+
+select
+
+fetchedAt   as  fetchedAt,
+metadata ->> "id" as Id
+(metadata -> "snippet" ->> "title" ) as title,
+(metadata -> "snippet" ->> "description") as descript,
+(metadata -> "snippet" ->> "publishedAt") as publishedAt,
+(metadata -> "statistics" ->> "likeCount")::int as like_count,
+(metadata -> "statistics" ->> "viewCount")::int as view_count,
+(metadata -> "statistics" ->> "favoriteCount")::int as favorite_count,
+(metadata -> "contentDetails" ->> "caption") as caption,
+(metadata -> "contentDetails" ->> "duration") as duration,
+
+
+from {{source('youtube', 'raw_videos')}}

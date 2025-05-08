fe3c0a49dac80b7e6212d83f0e3984a01055f4a3
moein fahmide <fahmide.moein@gmail.com>
2025-05-08 11:58:45 +0100

    small fixes, schema test

--- youtube_dbt/models/staging/stg_videos.sql
+++ youtube_dbt/models/staging/stg_videos.sql
@@ -4,15 +4,15 @@
 select
 
 fetchedAt   as  fetchedAt,
-metadata ->> "id" as Id
-(metadata -> "snippet" ->> "title" ) as title,
-(metadata -> "snippet" ->> "description") as descript,
-(metadata -> "snippet" ->> "publishedAt") as publishedAt,
-(metadata -> "statistics" ->> "likeCount")::int as like_count,
-(metadata -> "statistics" ->> "viewCount")::int as view_count,
-(metadata -> "statistics" ->> "favoriteCount")::int as favorite_count,
-(metadata -> "contentDetails" ->> "caption") as caption,
-(metadata -> "contentDetails" ->> "duration") as duration,
+metadata ->> 'id' as id,
+(metadata -> 'snippet' ->> 'title' ) as title,
+(metadata -> 'snippet' ->> 'description') as descript,
+(metadata -> 'snippet' ->> 'publishedAt') as publishedAt,
+(metadata -> 'statistics' ->> 'likeCount')::int as like_count,
+(metadata -> 'statistics' ->> 'viewCount')::int as view_count,
+(metadata -> 'statistics' ->> 'favoriteCount')::int as favorite_count,
+(metadata -> 'contentDetails' ->> 'caption') as caption,
+(metadata -> 'contentDetails' ->> 'duration') as duration
 
 
-from {{source('youtube', 'raw_videos')}}
+from raw.raw_videos

44e8a733a78c0ed92f1fef8bcfa8078fa61be700
moein fahmide <fahmide.moein@gmail.com>
2025-05-09 10:42:20 +0100

    new tables created: popular videos, corresponding channels

--- db/tables.py
+++ db/tables.py
@@ -17,4 +17,35 @@ def create_raw_videos_table():
     cn.execute(query)
     cn.commit()
 
+def create_raw_popular_videos_table():
+    cn = get_connection()
+    query = 'create schema if not exists raw'
+    cn.execute(query)
+    cn.commit()
 
+    query = """
+        create table if not exists raw.raw_popular_vidoes(
+            videoId     varchar     primary key,
+            fetchedAt   timestamptz not null default now(),
+            metadata    jsonb   not null
+        )
+        """
+    cn.execute(query)
+    cn.commit()
+    cn.close()
+
+def create_raw_channels_table():
+    cn = get_connection()
+    query = 'create schema if not exists raw'
+    cn.execute(query)
+    cn.commit()
+    query = """
+        create table if not exists raw.raw_channels(
+            channelId   varchar primary key,
+            fetchedAt   timestamptz not null default now(),
+            metadata     jsonb   not null
+        )
+    """
+    cn.execute(query)
+    cn.commit()
+    cn.close()
